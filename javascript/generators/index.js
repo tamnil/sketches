3b90f628e6a0efe20c0ae55ab3f0b2c3e9718c39
Tamnil Saito Junior <tamnil@gmail.com>
2020-01-02 17:51:15 -0300

    sync

--- javascript/generators/index.js
+++ javascript/generators/index.js
@@ -1,24 +1,31 @@
 // testing generators behavior
 //
 
-function* myGenFun(arg) {
-console.log('in yield1',arg)
-let y1 =     yield 1;
-console.log('in yeld2',arg,y1)
-let y2 =   yield* [100,3,87,62,2];
-console.log('in yeld3','yeld arguments',y2,y1)
-    yield 3;
-    yield 4;
+function* secGenFun() {
+    yield "inseide secGen fun";
 }
 
-let myGen = myGenFun('init arg')
+function* myGenFun(arg) {
+    console.log("in yield1", arg);
+    let y1 = yield 1;
+    console.log("in yeld2", arg, y1);
+    let y2 = yield* [100, 3, 87, 62, 2];
+    console.log("in yeld3", "yeld arguments", y2, y1);
+    yield secGenFun();
+    yield 3;
+    yield 4;
+    yield 5;
+    yield 6;
+}
 
-console.log(myGen)
-console.log(myGen.next('argData'))
-console.log(myGen.next('argData2    '))
-console.log(myGen.next())
-console.log(myGen.next())
-console.log(myGen.next())
-console.log(myGen.next())
-console.log(myGen.next())
-console.dir(myGen)
+let myGen = myGenFun("init arg");
+
+console.log(myGen);
+console.log(myGen.next("argData"));
+console.log(myGen.next("argData2    "));
+console.log(myGen.next());
+console.log(myGen.next());
+console.log(myGen.next());
+console.log(myGen.next());
+console.log(myGen.next());
+console.dir(myGen);
