69fc28e17c0299d59e3ae4019e293cb0de915bf9
Tamnil Saito Junior <tamnil@gmail.com>
2020-01-02 17:29:33 -0300

    generators

---
+++ javascript/generators/index.js
@@ -0,0 +1,24 @@
+// testing generators behavior
+//
+
+function* myGenFun(arg) {
+console.log('in yield1',arg)
+let y1 =     yield 1;
+console.log('in yeld2',arg,y1)
+let y2 =   yield* [100,3,87,62,2];
+console.log('in yeld3','yeld arguments',y2,y1)
+    yield 3;
+    yield 4;
+}
+
+let myGen = myGenFun('init arg')
+
+console.log(myGen)
+console.log(myGen.next('argData'))
+console.log(myGen.next('argData2    '))
+console.log(myGen.next())
+console.log(myGen.next())
+console.log(myGen.next())
+console.log(myGen.next())
+console.log(myGen.next())
+console.dir(myGen)
